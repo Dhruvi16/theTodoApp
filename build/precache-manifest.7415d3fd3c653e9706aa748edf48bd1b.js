171eaa454aad711000c1bbf09b56e77c89ba0169
dhruvi16 <dhruvibutti9477@gmail.com>
2020-11-01 21:41:55 +0530

    Add to production

---
+++ build/precache-manifest.7415d3fd3c653e9706aa748edf48bd1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c643c029dce007981d7",
+    "url": "/static/css/main.869177ae.chunk.css"
+  },
+  {
+    "revision": "4c643c029dce007981d7",
+    "url": "/static/js/main.12372f10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88b4e1a6074f8dcf01ca",
+    "url": "/static/css/2.403c440d.chunk.css"
+  },
+  {
+    "revision": "88b4e1a6074f8dcf01ca",
+    "url": "/static/js/2.5b233969.chunk.js"
+  },
+  {
+    "revision": "264712e9d3e58c88b017c178948420d5",
+    "url": "/index.html"
+  }
+];
